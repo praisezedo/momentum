141e9f96e48a5827fcda0f40aafa7fd34e28c5a0
Praise Innocent <praiseinnocent049@gmail.com>
2026-07-14 20:06:56 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl text-blue-900 font-bold justify-center flex min-h-screen items-center">
+        Momemtum   
+      </h1>  
+    </>
+  )
+}
+
+export default App
