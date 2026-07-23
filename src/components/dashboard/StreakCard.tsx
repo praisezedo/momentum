4e41ac4369002f22cdda84fc7a0990af2456ec47
Praisezedo <praiseinnocent049@gmail.com>
2026-07-23 15:34:10 +0100

    feat:added the increement streak button in dashboard

--- src/components/dashboard/StreakCard.tsx
+++ src/components/dashboard/StreakCard.tsx
@@ -1,11 +1,12 @@
-export default function StreakCard({streak}: {streak: number}) {
+export default function StreakCard({streak , setStreak}: {streak: number , setStreak: React.Dispatch<React.SetStateAction<number>>}) {
     return (
         <>
-       <div className="flex flex-col gap-4 justify-start ">
+       <div className="flex flex-col gap-4 justify-start">
                  <p className="text-2xl text-blue-900 ">🔥 Current Streak: <strong>{streak} days </strong></p>
                 <span className="text-shadow-xl text-blue-900 text-3xl">
                     keeps building momentum towards your  future.  
                 </span>
+                <button className="bg-white border border-blue-900 rounded-2xl text-blue-900 p-3 hover:cursor-pointer hover:bg-blue-900 hover:text-white" onClick={() => setStreak(streak + 1)}>Completed todays task</button>
          </div>
         </>
     )
--- src/pages/Dashboad.tsx
+++ src/pages/Dashboad.tsx
@@ -1,15 +1,17 @@
+import { useState } from "react";
 import StreakCard from "../components/dashboard/StreakCard";
 import WelcomeCard from "../components/dashboard/WelcomeCard";
 import Layout from "../components/layout/Layout";
 import {user} from "../data/user";
  
  export default function Dashboard() {
+  const [streak , setStreak] = useState(user.streak);
     return (
 
         <Layout>
              <section className="flex flex-col justify-center min-h-screen gap-7 items-center ">          
             <WelcomeCard  name={user.name}/>
-            <StreakCard streak={user.streak}/>
+            <StreakCard streak={streak} setStreak={setStreak}/>
           </section>
         </Layout>
 
