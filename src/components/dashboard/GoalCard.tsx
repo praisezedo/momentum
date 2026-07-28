5697905416963a14f7cae2ee3fe2a1d300462276
Praisezedo <praiseinnocent049@gmail.com>
2026-07-28 20:18:24 +0100

    refactor:  render dynamic goals using reusable GoalCard

--- src/components/dashboard/GoalCard.tsx
+++ src/components/dashboard/GoalCard.tsx
@@ -1,23 +1,10 @@
 
-type GoalCardProps = {
-    goals: string[];
-} 
-
-export default function GoalCard({ goals }: GoalCardProps) {
+export default function GoalCard({goal}: {goal: string}) {
     return (
         <>
-         <div className="flex-col-center">
-            {goals.length === 0 ? (<p>No goals added yet.</p>) 
-              : (
-            <ul className="grid grid-cols-3 gap-5 ">
-                {goals.map((goal: string , index: number) => (
-                     <li key={index} className="bg-blue-300 border-blue-500 border px-3 py-2 rounded-xl">
-                        <strong className="text-blue-900 text-xl">• {goal}</strong>
-                     </li>
-                ))}
-            </ul>
-            )}
-         </div>
+         <p className="text-blue-900 text-xl"> 
+            <strong>•{goal}</strong>
+         </p>
         </>
     )
 }
--- src/components/dashboard/GoalList.tsx
+++ src/components/dashboard/GoalList.tsx
@@ -6,7 +6,21 @@ export default function GoalList({goals}: {goals: string[]}) {
         <>
           <div className="flex-col-center gap-5">
               <h1 className="text-3xl text-blue-900 "><strong>{goals.length > 0 ? `${goals.length} ` : ''}</strong> Goals To Achieve 🔥</h1>
-               <GoalCard goals={goals} />
+
+            <div className="flex-col-center">
+                {goals.length <= 0 ? 
+                (<p className="text-black text-sm">No Goals Yet.</p>) 
+                : 
+                (
+                  <ul className="grid grid-cols-3 gap-5 ">
+                     {goals.map((goal: string , index: number ) => (
+                    <li key={index} className="bg-blue-300 border border-blue-500 rounded-xl px-3 py-2">
+                         <GoalCard goal={goal} />
+                    </li>
+                     ))}
+                  </ul>
+                )}
+            </div>    
           </div>
         </>
     )
