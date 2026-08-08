793443bdf92ed63b153926eb52801bdac81944a9
Praisezedo <praiseinnocent049@gmail.com>
2026-08-08 20:15:49 +0100

    feat: add goal search

--- src/components/dashboard/GoalList.tsx
+++ src/components/dashboard/GoalList.tsx
@@ -1,20 +1,41 @@
+import { useState } from "react";
 import type { Goal } from "../../types/goal";
 import GoalCard from "./GoalCard";
 
 
 export default function GoalList({goals , handleDeleteGoal , handleToggleGoal } : {goals: Goal[] , handleDeleteGoal: (id: number) => void , handleToggleGoal: (id: number) => void } ) {
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredGoals = goals.filter((goal) => 
+     goal.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
     return (
         <>
           <div className="flex-col-center gap-5">
-              <h1 className="text-3xl text-blue-900 "><strong>{goals.length > 0 ? `${goals.length} ` : ''}</strong> Goals To Achieve 🔥</h1>
+              <h1 className="text-3xl text-blue-900 "><strong>{filteredGoals.length > 0 ? `${filteredGoals.length} ` : ''}</strong> Goals To Achieve 🔥</h1>
+               
+               <div className="flex gap-7 my-7 justify-between items-center">
 
+                 <label className="text-blue-900 text-xl font-bold" htmlFor="search-goal-title">🔍 Search Goals:</label>
+                 <input 
+                 type="text" 
+                 id="search-goal-title"
+                  placeholder="Search Goals..."
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                  className="border border-gray-300 rounded-2xl px-3 py-2 min-w-100 focus:outline-none focus:border-blue-900"
+                 />
+
+               </div>
             <div className="flex-col-center">
-                {goals.length <= 0 ? 
-                (<p className="text-black text-sm">No Goals Yet.</p>) 
+                {filteredGoals.length === 0  ? 
+                   goals.length > 0 ? (<p className="text-black text-sm">No goals match the search criteria.</p>) : (<p className="text-black text-sm">No Goals Yet.</p>)
                 : 
                 (
                   <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 ">
-                     {goals.map((goal: Goal ) => (
+                     {filteredGoals.map((goal: Goal ) => (
                     <li key={goal.id} >
                          <GoalCard goal={goal} handleDeleteGoal={handleDeleteGoal} handleToggleGoal={handleToggleGoal} />
                     </li>
